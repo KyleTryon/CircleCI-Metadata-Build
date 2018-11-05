baf9f728967afbbfb3eee9b45fb6e33e6872765e
Kyle Tryon <33272306+KyleTryon@users.noreply.github.com>
2018-11-05 10:42:23 -0500

    add chrome options

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('/usr/bin/chromedriver')  # Optional argument, if not specified will search path.
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)  # Optional argument, if not specified will search path.
 # or '/usr/lib/chromium-browser/chromedriver' if you use chromium-chromedriver
 driver.get('http://www.google.com/xhtml');
 time.sleep(5) # Let the user actually see something!
